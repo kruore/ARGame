574b8c0deb0c7c81d855fef3aa8bac4e0582897d
kruore <kruore1321@gmail.com>
2019-04-22 14:59:34 +0900

    3
    3c

---
+++ ARProject/Assets/BattleManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BattleManager : NetworkBehaviour
+{
+    public enum Turn {StartGame,Drow,Battle,End};
+    public Turn turn = Turn.StartGame;
+    // Start is called before the first frame update
+    [Command]
+    public void CmdDrowCard()
+    {
+        switch (turn)
+        {
+            case Turn.StartGame:
+                Debug.Log("Start Battle");
+                break;
+            case Turn.Drow:
+                Debug.Log("Drow Card");
+                break;
+            case Turn.Battle:
+                Debug.Log("Battle");
+                break;
+            case Turn.End:
+                Debug.Log("End");
+                break;
+        }
+    }
+
+    [Command]
+    public void CmdTurnEnd()
+    {
+        if(!isLocalPlayer)
+        {
+            return;
+        }
+        turn++;
+        if(turn == Turn.End)
+        {
+            turn = Turn.StartGame;
+        }
+        CmdDrowCard();
+    }
+    public void Surrender()
+    {
+        Debug.Log("항복합니다.");
+    }
+}
