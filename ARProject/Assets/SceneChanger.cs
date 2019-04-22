b89e0b8e22336984d5d24118c77be6ad28c46db0
leesangjn <kruore1321@gmail.com>
2019-04-23 00:01:09 +0900

    5
    commit

---
+++ ARProject/Assets/SceneChanger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public static SceneChanger instance;
+    string scene_name;
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    void LoadScene()
+    {
+        SceneManager.LoadScene(scene_name);
+    }
+    void LateLoadScene()
+    {
+        Invoke("LoadScene", 0.5f);
+    }
+    public void Goto02()  //복사 씹가능
+    {
+        scene_name = "02";
+        LateLoadScene();
+    }
+}
--- ARProject/Assets/Scripts/GameManager.cs
+++ ARProject/Assets/Scripts/GameManager.cs
@@ -4,8 +4,56 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-    public GameManager gameManager;
+    public static GameManager gameManager;
 
+    enum PlayableCharacterelement {None, Stone,Grass,Tree};
+    PlayableCharacterelement playableCharacterelement = PlayableCharacterelement.None;
+    public void SelectCharacter()
+    {
 
-
+     switch(playableCharacterelement)
+        {
+            case PlayableCharacterelement.None:
+                Debug.Log("아무것도 결정되지 않았습니다.");
+                break;
+            case PlayableCharacterelement.Stone:
+                Debug.Log("돌을 골랐습니다.");
+                break;
+            case PlayableCharacterelement.Grass:
+                Debug.Log("풀을 골랐습니다.");
+                break;
+            case PlayableCharacterelement.Tree:
+                Debug.Log("나무을 골랐습니다.");
+                break;
+            default:
+                return;
+        }
+    }
+   public void SelectedStone()
+    {
+        playableCharacterelement = PlayableCharacterelement.Stone;
+        SelectCharacter();
+    }
+    public void SelectedGrass()
+    {
+        playableCharacterelement = PlayableCharacterelement.Grass;
+        SelectCharacter();
+    }
+    public void SelectedTree()
+    {
+        playableCharacterelement = PlayableCharacterelement.Tree;
+        SelectCharacter();
+    }
+    public void SelectedFinal()
+    {
+        if (playableCharacterelement == 0)
+        {
+            SelectCharacter();
+        }
+        if (playableCharacterelement!=0)
+        {
+            Debug.Log("해당 정령을 추가합니다 : " + playableCharacterelement);
+                //TODO: 해당 기능 추가할 것(DataBase)
+        }
+    }
 }
